33f7f622c6c5ad8aa59f238b560d083afc98ec82
Igna Luna <ignaluna98pb@gmail.com>
2023-08-16 10:52:04 -0300

    Scale Fernet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
         className={`absolute inset-0  max-w-7xl mx-auto ${styles.paddingX} flex flex-col lg:flex-row items-center justify-center gap-5`}
       >
         <div className="w-screen flex flex-col justify-center items-center">
-          <h1 className={`${styles.heroHeadText} w-full text-center`}>
-            VAMO ARRI<span className={`text-transparent bg-top bg-clip-text text-shadow-lg
+          <h1 className={`${styles.heroHeadText} w-full text-center transform transition-transform duration-300 hover:scale-110`}>
+            VAMO ARRI<span className={`text-transparent bg-top bg-clip-text text-shadow-lg 
           bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoZ__8T0wM9gK9_SN1kDdEILMmozmvyxMTJ8EqKW3&s')]`}>CBA</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-black-100 text-shadow-lg text-center`}>
             Experiencia inmersiva cordobesa, conocé la cultura del culiadaso.
           </p>
         </div>
-        <img src="/logo.png" alt="viajes a cordoba" className="transform-scale hover:scale-150"></img>
+        <img src="/logo.png" alt="viajes a cordoba" className="transform transition-transform duration-300 hover:scale-110"></img>
       </div>
     </section>
   );
